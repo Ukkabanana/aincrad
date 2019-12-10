60a29829cb3d6c23b193da0363774efd7838a5ac
Ukkabanana <arin.uk1409@gmail.com>
2019-12-10 17:53:17 +0700

    feat: toast&alert for registration

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth'
 import { auth } from 'firebase/app'
+import { ToastController } from '@ionic/angular' 
+import { AlertController } from '@ionic/angular'
 
 @Component({
   selector: 'app-register',
@@ -13,7 +15,11 @@ export class RegisterPage implements OnInit {
   password: string =""
   cpassword: string =""
 
-  constructor(public afAuth: AngularFireAuth) { }
+  constructor(
+    public afAuth: AngularFireAuth,
+    public toastCtrl: ToastController,
+    public alertCtrl: AlertController
+    ) { }
 
   ngOnInit() {
   }
@@ -21,21 +27,42 @@ export class RegisterPage implements OnInit {
   async register(){
     const { username, password, cpassword } = this
     if(password != cpassword){
+      this.showAlert("Error", "Password doesn't match")
       return console.error("Password doesn't match")
     }  
 
     try{
       const result = await this.afAuth.auth.createUserWithEmailAndPassword(username,password)
       console.log(result)
+      this.showToast("Success!", "Welcome to the realm!")
     } catch(error){
       console.dir(error)
       if(error.code=="auth/weak-password"){
         console.log("Your password is too weak. It should be at least 67 characters")
+        this.showAlert("Error", error.message)
       }
     }
 
 
     
+  }
+
+  async showToast(header: string, message: string){
+     const toast = await this.toastCtrl.create({
+       header,
+       message,
+       position: 'bottom',
+       duration: 2000
+     })
+     await toast.present()
+  }
+  async showAlert(header: string, message: string){
+    const alert = await this.alertCtrl.create({
+      header,
+      message,
+      buttons: ['Ok']
+    })
+    await alert.present()
   }
 
 }
