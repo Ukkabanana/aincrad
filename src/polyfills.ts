d63ed080e3b74002889459596d43dc00860c8296
Copter <copter12349@hotmail.com>
2019-12-12 00:40:28 +0700

    Display text on home.page.html from home.page.ts with mock XML

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -64,3 +64,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+// My Edit:
+// Needed for xml2js to work.
+(window as any).global = window;
