8f70a325fc6bf0db90b9a4e41a311d58f9edf02f
Ukkabanana <arin.uk1409@gmail.com>
2019-12-12 15:03:31 +0700

    add: review page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,11 @@ const routes: Routes = [
   {
     path: 'profile',
     loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  },  {
+    path: 'review',
+    loadChildren: () => import('./review/review.module').then( m => m.ReviewPageModule)
   },
+
 
 
 ];
