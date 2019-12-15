dc8f0ee9ab59641ae2ac46c635d0b9630ca593d6
Copter <copter12349@hotmail.com>
2019-12-15 21:57:08 +0700

    Today's Pick now display the 'hottest' game

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,7 +11,7 @@ import { TransformProvider } from '../providers/transform';
 export class BoardgameData {
     constructor(private http: HttpClient, private transformProvider: TransformProvider) {}
 
-    //Input string (Board game ID) to return data from public api (bgg).
+    //Input string (Board game ID) to get data from public api (BGG).
     getApi(id: string) {
         return this
             .http
@@ -23,6 +23,7 @@ export class BoardgameData {
             )
     }
 
+    //Input string (Search Text) to get data of all games containing input string in their names.
     searchApi(searchText: string) {
         console.log(searchText);
         return this
@@ -34,6 +35,19 @@ export class BoardgameData {
             map((result: string) => this.transformProvider.convertToJson(result))
             )
     }
+
+    //Get data of the top 50 games currently in the 'hot' section on BGG
+    getTop50() {
+        return this
+            .http
+            .get('https://www.boardgamegeek.com/xmlapi2/hot?type=boardgame', { responseType: "text" })   
+            // use the pipe method
+            .pipe(
+            // ... to apply oprtators to obervables
+            map((result: string) => this.transformProvider.convertToJson(result))
+            )
+    }
+    
     // Source: https://forum.ionicframework.com/t/how-convert-xml-input-file-to-json/40759/18
 
 }
