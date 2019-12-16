aae41209e20657ec636e395a6bdb59a07afc4665
Copter <copter12349@hotmail.com>
2019-12-16 19:14:43 +0700

    Show reviews under Details page

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BoardgameData } from '../services/data.service';
 
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UserService } from '../user.service';
+
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
@@ -12,7 +15,13 @@ export class DetailsPage implements OnInit {
   private idInUrl: string;
   result:any;
 
-  constructor(private route:ActivatedRoute, private bgData: BoardgameData) {}
+  showReviewArr: Array<any>;
+
+  constructor(private route:ActivatedRoute, 
+    private bgData: BoardgameData, 
+    public afs: AngularFirestore,
+    public user: UserService
+  ) {}
 
   ngOnInit() {
     //Get ID from URL
@@ -22,7 +31,7 @@ export class DetailsPage implements OnInit {
     //Get data of that game
     this.bgData.getApi(this.idInUrl).subscribe(data => {
       this.result = data;
-      console.log(data);  //FOR DEBUGGING
+      //console.log(data);  //FOR DEBUGGING
       // JSON converted from XML is not easy to read. This results in multiple-level nested JSON.
       let gameItem = this.result.items.item;
       // In XML provided by the public API, 'name' may or may not be an array.
@@ -41,6 +50,37 @@ export class DetailsPage implements OnInit {
       else  document.getElementById("gameTime").innerHTML = "Time: "+gameItem.minplaytime.$.value+" - "+gameItem.maxplaytime.$.value+" min.";
       document.getElementById("gameAge").innerHTML =  "Age: "+gameItem.minage.$.value+"+";
     });
+
+    //Get Reviews from Firebase
+    //Need another variable because of different access level.
+    var dataArr:Array<any> = [];
+    //Accessing review collection in Firebase
+    var reviewColl = this.afs.firestore.collection("review");
+    reviewColl.where("gameid", "==", this.idInUrl).get().then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.data());  //FOR DEBUGGING
+        //Number rating to Star rating
+        var starRating:string;
+        if(doc.data().rating == 1)  starRating="⭐";
+        else if(doc.data().rating == 2)  starRating="⭐⭐";
+        else if(doc.data().rating == 3)  starRating="⭐⭐⭐";
+        else if(doc.data().rating == 4)  starRating="⭐⭐⭐⭐";
+        else starRating="⭐⭐⭐⭐⭐";
+        //Put data in arrays
+        dataArr.push([
+          starRating,
+          doc.data().user,
+          doc.data().feedback,
+          doc.data().duration,
+          doc.data().group
+        ]);
+        
+      });
+    })
+    //Need another variable because of different access level.
+    this.showReviewArr = dataArr;
+    
   }
 
 }
